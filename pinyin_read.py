dd0e2c1a57069317f39c9a6104f40bede8d9d922
longfuxu <62403015+longfuxu@users.noreply.github.com>
2020-04-07 12:23:43 +0200

    Update pinyin_read.py
    verson 0.0.2 with only file reading

--- pinyin_read.py
+++ pinyin_read.py
@@ -10,8 +10,8 @@ pinyin_file = []
 for i in np.arange(len(pinyin)):
     pinyin_file = pinyin_file + pinyin[i]
 print(pinyin_file)
-# f.close()
+f.close()
 
 
-pinyin_name = pinyin('首孝弟  次见闻    知某数  识某文  一而十  十而百  百而千  千而万 路过 不巧 便条 派对 计划 邀请 抱歉 没法儿 正好 肯定 不许 前一天 惊喜 快乐')
-print(pinyin_name)
+# pinyin_name = pinyin('首孝弟  次见闻    知某数  识某文  一而十  十而百  百而千  千而万 路过 不巧 便条 派对 计划 邀请 抱歉 没法儿 正好 肯定 不许 前一天 惊喜 快乐')
+# print(pinyin_name)
