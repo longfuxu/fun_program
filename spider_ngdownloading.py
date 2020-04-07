48628c6f9866271c9a310151c15f45b9365a18d6
longfuxu <l2.xu@vu.nl>
2020-04-07 12:41:25 +0200

    correct the modules

--- spider_ngdownloading.py
+++ spider_ngdownloading.py
@@ -1,5 +1,6 @@
-# import requests
-# from bs4 import BeautifulSoup
+import requests
+from bs4 import BeautifulSoup
+# good job
 
 #找到正确且准确的母网页
 url_parent = 'http://www.ngchina.com.cn/travel/photo_galleries/4802.html'
